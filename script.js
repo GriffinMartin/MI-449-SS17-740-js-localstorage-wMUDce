0e15345ffa75f1a10fb1d187481ef3e69a57766f
Griffin Martin <mart1444@CABS-DEVSTUDENT1-MB.local>
2017-02-09 14:51:36 -0500

    refactored, added counter, day & night theme

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var fluxButton = document.getElementById('flux')
 
 function init () {
+  // Counter initiate and render
+  var visitCount = window.localStorage.getItem('counter')
+  document.getElementById('visits').innerHTML = visitCount
   window.localStorage.getItem('flux')
+  if (visitCount === null) {
+    visitCount = 0
+  }
+  visitCount++
+  window.localStorage.setItem('counter', visitCount)
+  // Retrieve and set localStorage class
   if (window.localStorage.getItem('flux') === 'off') {
     toggleOff()
   } else if (window.localStorage.getItem('flux') === 'on') {
@@ -9,24 +18,30 @@ function init () {
   }
 }
 
+// Toggles flux on and modifies localStorage
 function toggleOn () {
   window.localStorage.removeItem('flux', 'off')
   document.body.setAttribute('class', 'flux-on')
   window.localStorage.setItem('flux', 'on')
 }
 
+// Toggles flux off an modifies localStorage
 function toggleOff () {
-  document.body.setAttribute('class', 'flux-off')
   window.localStorage.removeItem('flux', 'on')
+  document.body.setAttribute('class', 'flux-off')
   window.localStorage.setItem('flux', 'off')
 }
 
+// Retrieves localStorage state and changes class each click
 fluxButton.addEventListener('click', function () {
-  if (window.localStorage.getItem('flux') === 'off') {
+  if (window.localStorage.getItem('flux') === null) {
+    toggleOn()
+  } else if (window.localStorage.getItem('flux') === 'off') {
     toggleOn()
   } else if (window.localStorage.getItem('flux') === 'on') {
     toggleOff()
   }
 })
 
+// Begin initialization
 init()
