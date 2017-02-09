786d29a560c29fc7d132bde9ed108710d17e32e3
Griffin Martin <mart1444@CABS-DEVSTUDENT1-MB.local>
2017-02-09 13:46:36 -0500

    localStorage working

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 var fluxButton = document.getElementById('flux')
-var on = window.localStorage.setItem('flux', on)
-var off = window.localStorage.removeItem('flux', off)
 
-fluxButton.addEventListener('click', function (on, off) {
-  if (on) {
-    document.body.setAttribute('class', 'flux-on')
-  } else if (off) {
-    document.body.setAttribute('class', 'flux-off')
+function init () {
+  window.localStorage.getItem('flux')
+  if (window.localStorage.getItem('flux') === 'off') {
+    toggleOff()
+  } else if (window.localStorage.getItem('flux') === 'on') {
+    toggleOn()
+  }
+}
+
+function toggleOn () {
+  window.localStorage.removeItem('flux', 'off')
+  document.body.setAttribute('class', 'flux-on')
+  window.localStorage.setItem('flux', 'on')
+}
+
+function toggleOff () {
+  document.body.setAttribute('class', 'flux-off')
+  window.localStorage.removeItem('flux', 'on')
+  window.localStorage.setItem('flux', 'off')
+}
+
+fluxButton.addEventListener('click', function () {
+  if (window.localStorage.getItem('flux') === 'off') {
+    toggleOn()
+  } else if (window.localStorage.getItem('flux') === 'on') {
+    toggleOff()
   }
 })
 
+init()
