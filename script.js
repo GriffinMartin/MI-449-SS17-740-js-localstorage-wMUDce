7ab275c41bc7c1dbd363b2536d9139290fe00088
GriffinMartin <gmart248@gmail.com>
2017-02-11 13:56:43 -0500

    feedback update

--- script.js
+++ script.js
@@ -6,42 +6,28 @@ function init () {
   if (visitCount === null) {
     visitCount = 0
   }
-  visitCount++
+  parseInt(visitCount++)
   window.localStorage.setItem('counter', visitCount)
   document.getElementById('visits').innerHTML = 'Total Visits: ' + visitCount
-  // Retrieve and set localStorage class
-  window.localStorage.getItem('flux')
-  if (window.localStorage.getItem('flux') === 'off') {
-    toggleOff()
-  } else if (window.localStorage.getItem('flux') === 'on') {
-    toggleOn()
+}
+
+function toggleFlux () {
+  var flux = window.localStorage.getItem('flux')
+  if (flux === 'on') {
+    window.localStorage.setItem('flux', 'off')
+  } else {
+    window.localStorage.setItem('flux', 'on')
   }
+  updateTheme()
 }
 
-// Toggles flux on and modifies localStorage
-function toggleOn () {
-  window.localStorage.removeItem('flux', 'off')
-  document.body.setAttribute('class', 'flux-on')
-  window.localStorage.setItem('flux', 'on')
-}
-
-// Toggles flux off an modifies localStorage
-function toggleOff () {
-  window.localStorage.removeItem('flux', 'on')
-  document.body.setAttribute('class', 'flux-off')
-  window.localStorage.setItem('flux', 'off')
+function updateTheme () {
+  var flux = window.localStorage.getItem('flux')
+  document.body.setAttribute('class', flux)
 }
 
 // Retrieves localStorage state and changes class each click
-fluxButton.addEventListener('click', function () {
-  if (window.localStorage.getItem('flux') === null) {
-    toggleOn()
-  } else if (window.localStorage.getItem('flux') === 'off') {
-    toggleOn()
-  } else if (window.localStorage.getItem('flux') === 'on') {
-    toggleOff()
-  }
-})
+fluxButton.addEventListener('click', toggleFlux)
 
 // Begin initialization
 init()
