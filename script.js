49ef35dcad1f45e738bcbd387dd76fc505e4edb1
GriffinMartin <gmart248@gmail.com>
2017-02-11 16:54:20 -0500

    refactoring and scope

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var fluxButton = document.getElementById('flux')
+var newflux = window.localStorage.getItem('flux')
+
+// Retrieves localStorage state and changes class each click
+fluxButton.addEventListener('click', toggleFlux)
 
 function init () {
   // Counter initiate and render
-  var visitCount = window.localStorage.getItem('counter')
-  if (visitCount === null) {
-    visitCount = 0
-  }
-  parseInt(visitCount++)
+  var visitCount = window.localStorage.getItem('counter' || 0)
+  visitCount++
   window.localStorage.setItem('counter', visitCount)
   document.getElementById('visits').innerHTML = 'Total Visits: ' + visitCount
 }
@@ -21,13 +22,19 @@ function toggleFlux () {
   updateTheme()
 }
 
-function updateTheme () {
-  var flux = window.localStorage.getItem('flux')
-  document.body.setAttribute('class', flux)
-}
+// Other ways to setItem
+/*
+Ternary:
+var newFlux = flux === 'on' ? 'off' : 'on'
+window.localStorage.setItem('flux', newFlux)
+Or:
+window.localStorage.setItem('flux', flux === 'on' ? 'off' : 'on')
+*/
 
-// Retrieves localStorage state and changes class each click
-fluxButton.addEventListener('click', toggleFlux)
+function updateTheme () {
+  var newflux = window.localStorage.getItem('flux')
+  document.body.setAttribute('class', newflux)
+}
 
 // Begin initialization
 init()
