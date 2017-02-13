cf2aa378b87f23f0251e06223d2f2354ab3bae75
Griffin Martin <mart1444@CABS-DEVSTUDENT1-MB.local>
2017-02-13 15:45:39 -0500

    counter syntax fix, updateTheme fix

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var fluxButton = document.getElementById('flux')
-var newflux = window.localStorage.getItem('flux')
 
 // Retrieves localStorage state and changes class each click
 fluxButton.addEventListener('click', toggleFlux)
 
 function init () {
+  // Set theme on initial load
+  updateTheme()
   // Counter initiate and render
-  var visitCount = window.localStorage.getItem('counter' || 0)
+  var visitCount = parseInt(window.localStorage.getItem('counter')) || 0
   visitCount++
   window.localStorage.setItem('counter', visitCount)
   document.getElementById('visits').innerHTML = 'Total Visits: ' + visitCount
@@ -20,6 +21,7 @@ function toggleFlux () {
     window.localStorage.setItem('flux', 'on')
   }
   updateTheme()
+  // console.log(window.localStorage.getItem('flux'))
 }
 
 // Other ways to setItem
@@ -31,9 +33,12 @@ Or:
 window.localStorage.setItem('flux', flux === 'on' ? 'off' : 'on')
 */
 
+// Changes CSS class to match localStorage
 function updateTheme () {
-  var newflux = window.localStorage.getItem('flux')
-  document.body.setAttribute('class', newflux)
+  var newFlux = window.localStorage.getItem('flux')
+  if (newFlux != null) {
+    document.body.setAttribute('class', newFlux)
+  }
 }
 
 // Begin initialization
